ffc5c9951fabec48b3c7fe862410fcebb861bbc5
Timotei Adam <timotei.adam@emanuel.ro>
2024-11-21 12:30:10 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
-base: '',
+    base: '',
+    build: {
+        rollupOptions: {
+            input: ["index.html", "login.html", "squad.html", "defenders.html", "midfielders.html", "forwards.html", "shedule.html", "matchinfo.html", "photos.html", "about us.html"]
+        }
+    }
 })
